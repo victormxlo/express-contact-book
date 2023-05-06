c37e5da93566279939924593af31daaef682f540
Victor Mello <joaovictortrajanomelo@gmail.com>
2023-05-06 13:11:21 -0300

    Implemented user login system

--- src/controllers/loginController.js
+++ src/controllers/loginController.js
@@ -1,7 +1,8 @@
 const Login = require('../models/LoginModel');
 
 exports.index = (req, res) => {
-    res.render('login.ejs');
+    if (req.session.user) return res.render('login-logged.ejs');
+    return res.render('login.ejs');
 };
 
 exports.register = async function(req, res) {
@@ -25,4 +26,33 @@ exports.register = async function(req, res) {
         console.log(e);
         return res.render('error-page.ejs'); 
     };
+};
+
+exports.login = async function(req, res) {
+    try {
+        const login = new Login(req.body);
+        await login.login();
+
+        if (login.errors.length > 0) {
+            req.flash('errors', login.errors);
+            req.session.save(function() {
+                return res.redirect('/login/index');
+            });
+            return;
+        };
+
+        req.flash('success', 'You are now logged in');
+        req.session.user = login.user;
+        req.session.save(function() {
+            return res.redirect('/login/index');
+        });
+    } catch (e) {
+        console.log(e);
+        res.render('error-page.ejs');
+    };
+};
+
+exports.logout = (req, res) => {
+    req.session.destroy();
+    res.redirect('/');
 };
